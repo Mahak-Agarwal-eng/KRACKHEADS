bd1219c3fcd372e95b2fdc403ca0f4b6716b82ae
Mahak-Agarwal-eng <mahakag2805@gmail.com>
2025-02-24 01:48:26 +0530

    Delete deployment_agent.py

--- deployment_agent.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def deploy_website(html_code):
-    url = "https://some-deployment-service.com/api/deploy"
-    response = requests.post(url, json={"html": html_code})
-    return response.json()
